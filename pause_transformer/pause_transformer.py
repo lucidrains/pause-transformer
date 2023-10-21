5e5de3207e0dc2654455e7b21f57644419f1dfb8
lucidrains <lucidrains@gmail.com>
2023-10-21 08:30:13 -0700

    quick causal transformer

--- pause_transformer/pause_transformer.py
+++ pause_transformer/pause_transformer.py
@@ -1,15 +1,114 @@
 import torch
 from torch import nn, Tensor, einsum
-from torch.nn import Module, ModuleList
+from torch.nn import Module, ModuleList, Sequential
 
 from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
 
 # functions
 
 def exists(v):
     return v is not None
 
+# norm
+
+class RMSNorm(Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.scale = dim ** 0.5
+        self.gamma = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        return F.normalize(x, dim = -1) * self.scale * self.gamma
+
+# feedforward
+
+def FeedForward(dim, mult = 4):
+    dim_inner = int(dim * mult)
+    return Sequential(
+        RMSNorm(dim),
+        nn.Linear(dim, dim_inner),
+        nn.GELU(),
+        nn.Linear(dim_inner, dim)
+    )
+
+class CausalAttention(Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.scale = dim ** -0.5
+        dim_inner = dim_head * heads
+
+        self.norm = RMSNorm(dim)
+        self.to_qkv = Sequential(
+            nn.Linear(dim, dim_inner * 3, bias = False),
+            Rearrange('b n (qkv h d) -> qkv b h n d', qkv = 3, h = heads)
+        )
+
+        self.to_out = Sequential(
+            Rearrange('b h n d -> b n (h d)'),
+            nn.Linear(dim_inner, dim, bias = False)
+        )
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        q, k, v = self.to_qkv(x)
+
+        q = q * self.scale
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        i, j = sim.shape[-2:]
+        causal_mask = torch.ones((i, j), device = x.device).triu(j - i + 1)
+
+        sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        return out
+
 # class
 
 class PauseTransformer(Module):
-    raise NotImplementedError
+    def __init__(
+        self,
+        *,
+        num_tokens,
+        dim,
+        depth,
+        dim_head = 64,
+        heads = 8,
+        ff_mult = 4
+    ):
+        super().__init__()
+
+        self.token_emb = nn.Embedding(num_tokens, dim)
+        self.layers = ModuleList([])
+
+        for _ in range(depth):
+            self.layers.append(ModuleList([
+                Attention(dim = dim, dim_head = dim_head, heads = heads),
+                FeedForward(dim = dim, mult = ff_mult)
+            ]))
+
+        self.to_logits = Sequential(
+            RMSNorm(dim),
+            nn.Linear(dim, num_tokens, bias = False)
+        )
+
+    def forward(self, x):
+        x = self.token_emb(x)
+
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        logits = self.to_logits(x)
+        return logits
